60671de91a8a12829d30a6b364cd1a7b140e1406
basit shahid <basitdogar59@gmail.com>
2026-09-12 22:58:04 +0500

    Stage 4: full CRUD

--- server.js
+++ server.js
@@ -78,6 +78,45 @@ missions.post('/missions',(req,res)=>{
     res.status(201).json(newmission);
 });
 
+
+
+missions.put('/missions/:id',(req,res)=>{
+    const missionID=parseInt(req.params.id);
+    const mission=missionslist.find(m=>m.missionID===missionID);
+
+    if(!mission){
+        return res.status(404).json({error:`Mission ${missionID} not found`});
+    }
+   
+    const { missionName, missionDescription } = req.body;
+
+    if (missionName === undefined && missionDescription === undefined) {
+        return res.status(400).json({ error: "Provide missionName and/or missionDescription" });
+    }
+
+    if (missionName !== undefined) mission.missionName = missionName;
+    if (missionDescription !== undefined) mission.missionDescription = missionDescription;
+
+    res.json(mission)
+
+
+})
+
+missions.delete('/missions/:id',(req,res)=>{
+
+    const missionID=parseInt(req.params.id);
+    const missionIndex=missionslist.findIndex(m=>m.missionID===missionID);
+    if(missionIndex===-1){
+        return res.status(404).json({error:`Mission ${missionID} not found`});
+
+    }
+
+    missionslist.splice(missionIndex,1);
+    res.status(204).send();
+
+});
+
+
 missions.listen(PORT,()=>{
     console.log(`server is active at http://localhost:${PORT}`);
     
