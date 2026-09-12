8c98e62009f9fed54fc643dd5bb54afc0d25124d
basit shahid <basitdogar59@gmail.com>
2026-09-12 21:45:54 +0500

    stage zero server's greeting

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const PORT=3000
 missions.get('/',(req,res)=>{
 res.send({message:"Hello, Server!"});
 });
+
+missions.listen(PORT,()=>{
+    console.log(`server is active at http://localhost:${PORT}`);
+    
+})
